fe7d15b65d8f818a59b1bf9fa773abb63aa48dde
hejeffery <553504116@qq.com>
2017-04-23 08:14:28 +0800

    完善

--- JBridge/JBridge/TestController.swift
+++ JBridge/JBridge/TestController.swift
@@ -7,29 +7,68 @@
 //
 
 import UIKit
+import JavaScriptCore
 
 class TestController: UIViewController {
+    
+    fileprivate var jsContext: JSContext?
 
+    private lazy var webView: UIWebView = {
+        let webView = UIWebView()
+        webView.delegate = self
+        
+        let path = Bundle.main.path(forResource: "test.html", ofType: nil)
+        if let path = path {
+            let url = URL(fileURLWithPath: path)
+            let request = URLRequest(url: url)
+            webView.loadRequest(request);
+        }
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addSubview(webView)
+        webView.frame = view.frame
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
+}
 
+extension TestController: UIWebViewDelegate {
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        print("------开始加载------")
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        print("------加载结束------")
+        title = webView.stringByEvaluatingJavaScript(from: "document.title")
+        
+        // 禁用用户选择
+        webView.stringByEvaluatingJavaScript(from: "document.documentElement.style.webkitUserSelect='none';")
+        // 禁用长按弹出框
+        webView.stringByEvaluatingJavaScript(from: "document.documentElement.style.webkitTouchCallout='none';")
+        
+        jsContext = webView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as? JSContext
+        
+        guard jsContext != nil else {
+            return
+        }
+        jsContext!.exceptionHandler = { (context, exception) -> () in
+            print("exception = \(String(describing: exception))")
+        }
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        print("------shouldStartLoadWith------")
+        return true
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print("------加载失败------")
+    }
+    
 }

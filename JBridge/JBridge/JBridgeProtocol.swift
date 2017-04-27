db39af611ca5be390470889e929babd1b545f272
hejeffery <553504116@qq.com>
2017-04-27 13:18:08 +0900

    完善

---
+++ JBridge/JBridge/JBridgeProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  JBridgeProtocol.swift
+//  JBridge
+//
+//  Created by HeJeffery on 2017/4/27.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+import Foundation
+import JavaScriptCore
+
+/** 
+ JBridgeProtocol protocol
+*/
+@objc public protocol JBridgeProtocol: JSExport {
+
+    static func showAlert(_ title: String, message: String);
+}

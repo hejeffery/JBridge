673244e303d064720e8332f1241f4a3aa329c003
hejeffery <553504116@qq.com>
2017-05-07 21:52:10 +0800

    调整

---
+++ JBridge/JBridge/JBridgeController.swift
@@ -0,0 +1,21 @@
+//
+//  JBridgeController.swift
+//  JBridge
+//
+//  Created by HeJeffery on 2017/5/7.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+import UIKit
+
+class JBridgeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}

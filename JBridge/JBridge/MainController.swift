787cc125cb79032d37f732f8cc8bdc300435e542
hejeffery <553504116@qq.com>
2017-06-18 21:38:19 +0800

    调整

--- JBridge/JBridge/MainController.swift
+++ JBridge/JBridge/MainController.swift
@@ -72,6 +72,7 @@ extension MainController: UIWebViewDelegate {
         jbridge.webView = webView
         // JS可以通过"jbridge"这个引用调用JBridge实例中的方法
         jscontext.setObject(jbridge, forKeyedSubscript: "jbridge" as (NSCopying & NSObjectProtocol)!)
+        
     }
     
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
